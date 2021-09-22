b951932b3dc84553cd2e3aa68ad981d7ff8e2f3c
jimmyyang <wu0h961738@yahoo.com.tw>
2021-09-23 07:44:12 +0800

    do nothing

---
+++ src/main/java/com/g0y/auth/exception/ControllerExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.g0y.auth.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@ResponseBody
+public class ControllerExceptionHandler {
+
+//    @ExceptionHandler()
+//    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+//    public Errorma
+
+}
